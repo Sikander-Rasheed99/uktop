1de837ae8d5e577f388d527a26259d48aeb3fb58
Sikander-Rasheed99 <sikanderrasheed39@gmail.com>
2023-06-12 01:39:07 -0700

    working on

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,56 @@
 
 $('.client-slider').slick({
-    speed: '1000',
-  //   autoplay: true,
+  autoPlay: true,
+  speed: '1000',
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  //  centerMode: true
+  arrows: false,
+  dots: false,
+});
+
+$(document).ready(function () {
+  $(".expert-slider").slick({
     slidesToShow: 1,
-     slidesToScroll: 1,
+    slidesToScroll: 1,
     arrows: false,
-    dots: false,
-    responsive: [{
-      breakpoint: 1000,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-      },
-      breakpoint: 650,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-      }
-    }]
-  
+    infinite: false,
+    autoplay: false
   });
+  $(".prev-btn").click(function () {
+    $(".expert-slider").slick("slickPrev");
+  });
+
+  $(".next-btn").click(function () {
+    $(".expert-slider").slick("slickNext");
+  });
+  $(".prev-btn").addClass("slick-disabled");
+  $(".expert-slider").on("afterChange", function () {
+    if ($(".slick-prev").hasClass("slick-disabled")) {
+      $(".prev-btn").addClass("slick-disabled");
+    } else {
+      $(".prev-btn").removeClass("slick-disabled");
+    }
+    if ($(".slick-next").hasClass("slick-disabled")) {
+      $(".next-btn").addClass("slick-disabled");
+    } else {
+      $(".next-btn").removeClass("slick-disabled");
+    }
+  });
+});
+
+
+
+// input tel start
+
+
+var input = document.querySelector("#phone");
+var iti = window.intlTelInput(input, {
+  // separateDialCode:true,
+  utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@18.1.0/build/js/utils.js",
+});
+
+// store the instance variable so we can access it in the console e.g. window.iti.getNumber()
+window.iti = iti;
+
+// input tel end 
